dc4e19fad7ed0f05efd107f8be2578402073594b
Marten Schulz <ms@marten-schulz.de>
2024-06-17 21:12:18 +0200

    Refactor urls.py to remove commented out code for create_restaurant and edit_restaurant paths

--- urls.py
+++ urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('new/', views.create_restaurant, name='create_restaurant'),
-    path('list/', views.restaurant_list, name='restaurant_list'),
-    #path('edit/<int:id>/', views.edit_restaurant, name='edit_restaurant'),  # URL für Bearbeiten
 ]
